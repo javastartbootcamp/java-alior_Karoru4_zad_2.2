995683fdd31993540e4ec76c6084aa21ea10f6d6
Karoru4 <Karl2188@>
2023-11-07 20:56:19 +0100

    commit 2

---
+++ src/main/java/Zadanie22.java
@@ -0,0 +1,13 @@
+public class Zadanie22 {
+    public static void main(String[] args) {
+        String product1 = "Mleko";
+        String producer1 = "Mlekowita";
+        double price1 = 2.50;
+        String product2 = "Czekolada";
+        String producer2 = "Wedel";
+        double price2 = 2.19;
+
+        System.out.println(product1 + " " + producer1 + " " + price1 + "zł");
+        System.out.println(product2 + " " + producer2 + " " + price2 + "zł");
+    }
+}
